e6224b81d59174507a82cbf24183de3bed9baad7
JADPA-23 <JADPA-23@JADPA-23>
2022-08-04 11:49:53 -0600

    Agregar archivos de proyecto.

---
+++ Pago/Form1.cs
@@ -0,0 +1,40 @@
+namespace Pago
+{
+    public partial class FrmPagoDeEmpleados : Form
+    {
+        public FrmPagoDeEmpleados()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
